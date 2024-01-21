f79ae3bbb280e9fe0b68f8b2fa3d466c7ebb63ca
m-jishnu <jishnum499@gmail.com>
2024-01-21 16:14:42 +0530

    bumped version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 DESCRIPTION = "A concurrent python download manager"
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
